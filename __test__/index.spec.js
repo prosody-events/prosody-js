86f87944738d4b97986206709ecab0a77183da09
Joshua Griffith <josh.griffith@gmail.com>
2026-08-10 19:48:55 -0500

    test(peer): override inherited bind address

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,7 +1,7 @@
 const { Readable } = require("stream");
 const { EventEmitter } = require("events");
 
-process.env.PROSODY_PEER_BIND_ADDRESS ??= "127.0.0.1:0";
+process.env.PROSODY_PEER_BIND_ADDRESS = "127.0.0.1:0";
 
 const {
   ConsumerState,
